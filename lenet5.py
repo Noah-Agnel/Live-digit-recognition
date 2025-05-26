3c91524bcfd892c24b9be39e81cea4480c9df540
Noah-Agnel <138210920+Noah-Agnel@users.noreply.github.com>
2025-05-26 17:12:22 +0200

    Add files via upload

---
+++ lenet5.py
@@ -0,0 +1,106 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import MaxPooling2D
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+rows, cols = 28, 28
+
+x_train = x_train.reshape(x_train.shape[0], rows, cols, 1)
+x_test = x_test.reshape(x_test.shape[0], rows, cols, 1)
+
+input_shape = (rows,cols,1) 
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+model = tf.keras.Sequential([
+# Layer 1
+Conv2D(filters = 32, kernel_size = 5, strides = 1, activation = 'relu', input_shape = input_shape, kernel_regularizer=l2(0.0005)),
+# Layer 2
+Conv2D(filters = 32, kernel_size = 5, strides = 1, use_bias=False),
+# Layer 3
+BatchNormalization(),
+# — — — — — — — — — — — — — — — — #
+Activation('relu'),
+MaxPooling2D(pool_size = 2, strides = 2),
+Dropout(0.25),
+# — — — — — — — — — — — — — — — — #
+# Layer 3
+Conv2D(filters = 64, kernel_size = 3, strides = 1, activation = 'relu', kernel_regularizer=l2(0.0005)),
+# Layer 4
+Conv2D(filters = 64, kernel_size = 3, strides = 1, use_bias=False),
+# Layer 5
+BatchNormalization(),
+# — — — — — — — — — — — — — — — — #
+Activation('relu'),
+MaxPooling2D(pool_size = 2, strides = 2),
+Dropout(0.25),
+Flatten(),
+# — — — — — — — — — — — — — — — — #
+# Layer 6
+Dense(units = 256, use_bias=False),
+# Layer 7
+BatchNormalization(),
+# — — — — — — — — — — — — — — — — #
+Activation('relu'),
+# — — — — — — — — — — — — — — — — #
+# Layer 8
+Dense(units = 128, use_bias=False),
+# Layer 9
+BatchNormalization(),
+# — — — — — — — — — — — — — — — — #
+Activation('relu'),
+# — — — — — — — — — — — — — — — — #
+# Layer 10
+Dense(units = 84, use_bias=False),
+# Layer 11
+BatchNormalization(),
+# — — — — — — — — — — — — — — — — #
+Activation('relu'),
+Dropout(0.25),
+# — — — — — — — — — — — — — — — — #
+# Output
+Dense(units = 10, activation = 'softmax')
+])
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.1),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# We will be allowing 10 itterations to happen
+epochs = 10
+model.fit(x_train, y_train, epochs=epochs,batch_size=128, verbose=1)
+
+# Check Accuracy of the Model
+loss ,acc= model.evaluate(x_test, y_test)
+print('Accuracy : ', acc)
+
+x_train = x_train.reshape(x_train.shape[0], 28,28)
+print('Training Data', x_train.shape, y_train.shape)
+x_test = x_test.reshape(x_test.shape[0], 28,28)
+print('Test Data', x_test.shape, y_test.shape)
+
+# Plot the Image
+image_index = 8888
+plt.imshow(x_test[image_index].reshape(28,28), cmap='Greys')
+# Make Prediction
+pred = model.predict(x_test[image_index].reshape(1, rows, cols, 1 ))
+model.save("model.h5")
+print(pred.argmax())
